854affaff8768cd2ac57006f7654d8a68428631e
tHeAmSiS <59922218+tHeAmSiS@users.noreply.github.com>
2026-02-14 13:18:24 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='linguamon-alpha-v1';
+const CACHE='linguamon-alpha-v2';
 const ASSETS=[
   './','./index.html','./style.css','./app.js','./manifest.json',
   './content/content.json','./content/version.json',
